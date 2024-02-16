93adc4060231460d967644efbc24bf5dda9bcbde
Andrew Nemec <andrewnemec1@gmail.com>
2024-02-16 17:28:12 -0500

    Implement grid initialization and add test code

--- grid.go
+++ grid.go
@@ -1,14 +1,25 @@
 package main
 
 type Grid struct {
+	Grid [][]*Cell
 	Rows int
 	Cols int
 }
 
 func NewGrid(rows, cols int) Grid {
+	g := prepareGrid(rows, cols)
 	grid := Grid{
+		g,
 		rows,
 		cols,
+	}
+	return grid
+}
+
+func prepareGrid(rows, cols int) [][]*Cell {
+	grid := make([][]*Cell, rows)
+	for i := range grid {
+		grid[i] = make([]*Cell, cols)
 	}
 	return grid
 }
--- grid_test.go
+++ grid_test.go
@@ -11,4 +11,20 @@ func TestGridSetup(t *testing.T) {
 				t.Errorf("got %d rows, wanted %d rows", grid.Rows, 2)
 			}
 		})
+
+	t.Run("Tests grid rows are defined properly", func(t *testing.T) {
+		grid := NewGrid(2, 2)
+
+		if len(grid.Grid) != 2 {
+			t.Errorf("got %d but wanted %d", len(grid.Grid), 2)
+		}
+	})
+
+	t.Run("Tests grid cols are defined properly", func(t *testing.T) {
+		grid := NewGrid(2, 2)
+
+		if len(grid.Grid[0]) != 2 {
+			t.Errorf("got %d but wanted %d", len(grid.Grid[0]), 2)
+		}
+	})
 }

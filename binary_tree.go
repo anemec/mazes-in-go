76c2e6285a19c1ce12fe06dd6ef5c722cf3be9f3
Andrew Nemec <andrewnemec2@gmail.com>
2024-03-03 20:31:51 -0500

    Add binary tree maze implementation

---
+++ binary_tree.go
@@ -0,0 +1,28 @@
+package main
+
+import "math/rand"
+
+type BinaryTree struct {
+	g Grid
+}
+
+func (bt BinaryTree) On() {
+	for cell := range bt.g.EachCell() {
+		neighbors := []*Cell{}
+
+		if cell.North != nil {
+			neighbors = append(neighbors, cell.North)
+		}
+		if cell.East != nil {
+			neighbors = append(neighbors, cell.East)
+		}
+
+		if len(neighbors) > 0 {
+			index := rand.Intn(len(neighbors))
+
+			neighbor := neighbors[index]
+
+			cell.Link(neighbor, true)
+		}
+	}
+}
